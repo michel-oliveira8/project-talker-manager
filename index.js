21932c5a635d8ad2771d31d12a7be718fb6c66c1
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-01-14 17:12:47 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,26 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkerList = await fs.readFile('./talker.json', 'utf-8')
+  .then((response) => JSON.parse(response));
+  if (talkerList.length === 0) return res.status(200).json([]);
+  res.status(200).json(talkerList);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await fs.readFile('./talker.json', 'utf-8')
+  .then((response) => JSON.parse(response));
+
+  const talkerId = talkers.find((t) => t.id === parseInt(id, 10));
+
+  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerId);
 });
 
 app.listen(PORT, () => {
